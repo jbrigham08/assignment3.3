93c3bc160e5a1b8cd8315e9328fedb6417d204fa
Software Engineer <SoftwareEngineer@MacBook-Air.attlocal.net>
2023-07-18 14:16:09 -0500

    assignment3.3

---
+++ assignment3.3/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './Counter'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+    <h1>Counter</h1>
+    <Counter />
+    </div>
+  )
+}
+
+export default App
---
+++ assignment3.3/src/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const handlerPlus = () => {
+        setCount(prevCount =>{
+            return (prevCount + 1);
+        });
+    };
+    
+    const handlerMinus = () => {
+        if(count === 0) return (0);
+        else setCount(prevCount =>{
+            return (prevCount - 1);
+        });
+    };
+
+    const handlerReset = () => {
+        setCount(prevCount =>{
+            return (0);
+        });
+    };
+
+
+    return (
+      <>
+        <button onClick={handlerMinus}>-</button>
+        <span>{count}</span>
+        <button onClick={handlerPlus}>+</button>
+        <button onClick={handlerReset}>Reset</button>   
+      </>
+    );
+  }
+  export default Counter;
